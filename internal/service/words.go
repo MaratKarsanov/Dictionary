befdf7d5d53bece5c3d813306b86779648f914d6
MaratKarsanov <marat.karsanov@mail.ru>
2025-09-12 14:19:21 +0300

    added update and delete endpoints

--- internal/service/words.go
+++ internal/service/words.go
@@ -47,3 +47,48 @@ func (s *Service) CreateWords(c echo.Context) error {
 
 	return c.String(http.StatusOK, "OK")
 }
+
+// UpdateWord обновляем слово в базе
+// localhost:8000/updateword/:id
+func (s *Service) UpdateWord(c echo.Context) error {
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		s.logger.Error(err)
+		return c.JSON(s.NewError(InvalidParams))
+	}
+
+	title := c.QueryParam("title")
+	translate := c.QueryParam("translation")
+	if title == "" || translate == "" {
+		s.logger.Error(err)
+		return c.JSON(s.NewError(InvalidParams))
+	}
+
+	repo := s.wordsRepo
+	err2 := repo.UpdateWord(id, title, translate)
+	if err2 != nil {
+		s.logger.Error(err)
+		return c.JSON(s.NewError(InternalServerError))
+	}
+
+	return c.String(http.StatusOK, "OK")
+}
+
+// DeleteWord удаляем слово из базы
+// localhost:8000/deleteword/:id
+func (s *Service) DeleteWord(c echo.Context) error {
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		s.logger.Error(err)
+		return c.JSON(s.NewError(InvalidParams))
+	}
+
+	repo := s.wordsRepo
+	err2 := repo.DeleteWordById(id)
+	if err2 != nil {
+		s.logger.Error(err)
+		return c.JSON(s.NewError(InternalServerError))
+	}
+
+	return c.String(http.StatusOK, "OK")
+}
--- internal/words/data.go
+++ internal/words/data.go
@@ -1,6 +1,9 @@
 package words
 
-import "database/sql"
+import (
+	"database/sql"
+	"errors"
+)
 
 type Repo struct {
 	db *sql.DB
@@ -25,6 +28,31 @@ func (r *Repo) RGetWordById(id int) (*Word, error) {
 // CreateNewWords добавляет новые переводы в базу даных
 func (r *Repo) CreateNewWords(word, translate string) error {
 	_, err := r.db.Exec(`INSERT INTO ru_en (title, translation) VALUES ($1, $2)`, word, translate)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// UpdateWord обновляет слово
+func (r *Repo) UpdateWord(id int, word, translate string) error {
+	res, err := r.db.Exec(`UPDATE ru_en SET title = $1, translation = $2 WHERE id = $3`, word, translate, id)
+	if err != nil {
+		return err
+	}
+
+	rowsAffected, _ := res.RowsAffected()
+	if rowsAffected == 0 {
+		return errors.New("WordNotFound")
+	}
+
+	return nil
+}
+
+// DeleteWordById удаляет слово по id
+func (r *Repo) DeleteWordById(id int) error {
+	_, err := r.db.Exec(`DELETE FROM ru_en WHERE id = $1`, id)
 	if err != nil {
 		return err
 	}
